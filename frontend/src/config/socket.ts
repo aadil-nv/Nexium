1667926e64bba1500b889a23616cecb39a17dd79
aadil-nv <adilev2000@gmail.com>
2025-02-03 16:50:29 +0530

    changed socket connection v2

--- frontend/src/config/socket.ts
+++ frontend/src/config/socket.ts
@@ -1,5 +1,4 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://backend.aadil.online/socket");
-
+const socket = io("https://backend.aadil.online/communication-service/socket");
 export default socket;
