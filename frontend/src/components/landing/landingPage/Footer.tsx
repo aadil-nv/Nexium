63f55ea11528e7979fc93adba2435a57d56699fe
aadil-nv <adilev2000@gmail.com>
2025-02-04 20:28:54 +0530

    fronten deployment bug fixed

--- frontend/src/components/landing/landingPage/Footer.tsx
+++ frontend/src/components/landing/landingPage/Footer.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { motion } from 'framer-motion';
 import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from 'react-icons/fa';
 import images from "../../../images/images";
